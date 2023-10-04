2d6d316cdab70193d0a670690b4057f011dc759f
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-04 11:46:54 +0800

    added loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+# for i in [1, 2, 3, 4, 5]:
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+
+# names = ["Jimzon", "Kheala", "Luffy"]
+# for name in names:
+#     print(name)
+
+name = "Jimzon"
+for str in name:
+    print(str)
